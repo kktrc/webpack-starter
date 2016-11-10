16c1aa136894be111325e0f613c9b5ddfb2bc378
pzzls <moshenglei@icloud.com>
2016-11-10 23:37:39 +0800

    add

--- app/sub.js
+++ app/sub.js
@@ -1,7 +1,17 @@
-function generateText(){
+// es 2015
+// function generateText(){
+//   var element = document.createElement('h2');
+//   element.innerHTML = "Hello h2 world";
+//   return element;
+// }
+//
+// module.exports = generateText;
+
+
+// es 2016
+
+export default function() {
   var element = document.createElement('h2');
-  element.innerHTML = "Hello h2 world";
+  element.innerHTML = "Hello h2 world hahaha";
   return element;
 }
-
-module.exports = generateText;
