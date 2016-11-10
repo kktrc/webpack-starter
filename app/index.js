3605892c0b32853a77f4bddaa8a1e18439276fdc
pzzls <moshenglei@icloud.com>
2016-11-10 23:03:21 +0800

    init

---
+++ app/index.js
@@ -0,0 +1,8 @@
+require('./main.css');
+require('./main.scss');
+
+var sub = require('./sub');
+var app = document.createElement('div');
+app.innerHTML = '<h1>Hello World</h1>';
+app.appendChild(sub());
+document.body.appendChild(app);
